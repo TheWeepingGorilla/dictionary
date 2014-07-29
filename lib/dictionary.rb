f3916db34553c99078dca234976ad0b7f4cc7f90
Sean Morrison & Sterling Houghton <student@epicodus.com>
2014-07-29 15:59:29 -0700

    Initial commit.  Term class added.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,4 +1,16 @@
 class Term
+
   def initialize (word, definition)
+    @word = word
+    @definition = definition
   end
+
+  def word
+    @word
+  end
+
+  def definition
+    @definition
+  end
+
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,4 +7,13 @@ describe 'Term' do
     test_term = Term.new("fish", "swimmy thing")
     test_term.should be_an_instance_of Term
   end
+
+  it 'returns correct word' do
+    test_term = Term.new("fish", "swimmy thing")
+    test_term.word.should eq "fish"
+  end
+    it 'returns correct definition' do
+    test_term = Term.new("fish", "swimmy thing")
+    test_term.definition.should eq "swimmy thing"
+  end
 end
